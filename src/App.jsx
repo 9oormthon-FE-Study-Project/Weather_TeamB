28dadceed008e8e11d248a5971d0d183eb5d74a0
smsdia412 <smsdia412@gmail.com>
2025-05-29 11:37:48 +0000

    feat/첫 화면 설정

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import MapForm from '../src/map/forms/MapPage'
+
+function App() {
+  return (
+    <div className="App">
+      <MapForm />
+    </div>
+  )
+}
+export default App
