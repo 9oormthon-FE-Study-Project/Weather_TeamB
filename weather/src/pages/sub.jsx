e6f922486c5c2a044a2a1ed05c7a2cbd5bfac2f3
seongjun-Bang <seongjun5897@gmail.com>
2025-06-13 13:45:34 +0900

    list 이동

--- weather/src/pages/sub.jsx
+++ weather/src/pages/sub.jsx
@@ -12,7 +12,7 @@ const Subpage = () => {
       <h1 className="text-6xl font-bold mb-32 max-w-1/2 flex">Weatherpeek</h1>
       <div className="max-w-xl w-full flex flex-col gap-20">
         {/* max-w-xl = 최대 너비/ w-full= 최대너비에 맞춰서 */}
-        <Button >날씨목록으로 가기</Button>
+        <Button onClick={()=> {navigate('/list')}}>날씨목록으로 가기</Button>
         <Button onClick={()=> {navigate('/map')}}>날씨지도로 가기</Button>
       </div>
     </div>
